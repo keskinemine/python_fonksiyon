90e925c3fe9a46921621d849d4d6eccb0bd4129a
keskinemine <85511772+keskinemine@users.noreply.github.com>
2021-11-07 21:05:46 +0300

    Add files via upload

---
+++ Pythonda Koşul İfadeleri/if-else.py
@@ -0,0 +1,12 @@
+username = 'eminekeskin'
+password = '1234'
+
+if (username == 'eminekeskin'):
+
+    if (password == '1234'):
+        print('Hoşgeldiniz')
+    else:
+        print('Parola yanlış')    
+        
+else:
+    print('Kullanıcı adı yanlış')
